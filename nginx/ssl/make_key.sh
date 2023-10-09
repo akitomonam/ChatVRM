e882c3ebd31d937460b3506c42b9b19cbb84bd20
akitomonam <akitomonam@gmail.com>
2023-10-09 15:21:37 +0900

    nginxのssl署名作成の修正

--- nginx/ssl/make_key.sh
+++ nginx/ssl/make_key.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
+DIR=$(dirname "$(readlink -f "$0")")
+
 openssl req -batch -new -x509 -newkey rsa:4096 -nodes -sha256 \
   -subj /CN=example.com/O=example -days 3650 \
-  -keyout ./server.key \
-  -out ./server.crt
+  -keyout "$DIR/server.key" \
+  -out "$DIR/server.crt"

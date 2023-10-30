050d1ffd752f326b9b7a8c4908ea415f9b73fd35
akitomonam <akitomonam@gmail.com>
2023-10-30 13:58:41 +0900

    学内用にポートとエンドポイントの修正

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || "",
-  basePath: process.env.BASE_PATH || "",
+  // assetPrefix: process.env.BASE_PATH || "",
+  // basePath: process.env.BASE_PATH || "",
+  assetPrefix: '/ced-iot',
+  basePath: '/ced-iot', 
   trailingSlash: true,
   publicRuntimeConfig: {
-    root: process.env.BASE_PATH || "",
+    // root: process.env.BASE_PATH || "",
+    root: '/ced-iot'
   },
   optimizeFonts: false,
 };

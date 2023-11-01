9a089c2f8317dcf793fb9056dba4c06063421323
akitomonam <akitomonam@gmail.com>
2023-11-01 13:53:01 +0900

    エンドポイントの修正

--- src/features/messages/synthesizeVoice.ts
+++ src/features/messages/synthesizeVoice.ts
@@ -30,7 +30,7 @@ export async function synthesizeVoiceApi(
     apiKey: apiKey,
   };
 
-  const res = await fetch("/api/tts", {
+  const res = await fetch("/ced-iot/api/tts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
